fc88c06ed44241568710b0143fb3dcc4337cafca
hiyosilver <130156584+hiyosilver@users.noreply.github.com>
2025-02-10 13:39:52 +0100

    Limit text fields to 45 characters + person panel fix

--- Animalshelter/src/gui/ShelterTextField.java
+++ Animalshelter/src/gui/ShelterTextField.java
@@ -1,31 +1,58 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 
 public class ShelterTextField extends JTextField implements GUIConstants {
+	private final int MAX_LENGTH = 45;
 	
 	public ShelterTextField(String string) {
-		this();
-		setText(string);
+		super(string);
+		setFont(FONT_TEXTFIELD);
+		addKeyListener(new KeyAdapter() {
+		    public void keyTyped(KeyEvent e) { 
+		        if(getText().length() >= MAX_LENGTH)
+		            e.consume(); 
+		    }  
+		});
 	}
 	
 	public ShelterTextField() {
 		super();
 		setFont(FONT_TEXTFIELD);
+		addKeyListener(new KeyAdapter() {
+		    public void keyTyped(KeyEvent e) { 
+		        if(getText().length() >= MAX_LENGTH)
+		            e.consume(); 
+		    }  
+		});
 	}
 
 	public ShelterTextField(int i) {
 		super(i);
 		setFont(FONT_TEXTFIELD);
+		addKeyListener(new KeyAdapter() {
+		    public void keyTyped(KeyEvent e) { 
+		        if(getText().length() >= MAX_LENGTH)
+		            e.consume(); 
+		    }  
+		});
 	}
 
 	public ShelterTextField(String string, int i) {
 		super(string, i);
 		setFont(FONT_TEXTFIELD);
+		addKeyListener(new KeyAdapter() {
+		    public void keyTyped(KeyEvent e) { 
+		        if(getText().length() >= MAX_LENGTH)
+		            e.consume(); 
+		    }  
+		});
 	}
 	
 	/**
