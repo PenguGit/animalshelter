ef922394b78a08b175b2795feec60765278ef56b
PenguGit <160494880+PenguGit@users.noreply.github.com>
2025-02-11 10:29:20 +0100

    New SQL

--- Animalshelter/src/gui/ShelterImagePanel.java
+++ Animalshelter/src/gui/ShelterImagePanel.java
@@ -1,14 +1,11 @@
 package gui;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class ShelterImagePanel extends JPanel {
