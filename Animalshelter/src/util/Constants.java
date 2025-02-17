8ca338457644e1477d1ad3bd25bf07d48181cfda
PenguGit <160494880+PenguGit@users.noreply.github.com>
2025-02-17 13:34:19 +0100

    Changed location

--- Animalshelter/src/util/Constants.java
+++ Animalshelter/src/util/Constants.java
@@ -2,7 +2,7 @@ package util;
 
 public class Constants {
 	//public static final String DB_URL = "jdbc:mysql://localhost:3306/animalshelter";
-	public static final String DB_URL = "jdbc:sqlite:animalshelter.db";
+	public static final String DB_URL = "jdbc:sqlite:resources\\db\\animalshelter.db";
 //	public static final String DB_USER = "root";
 //	public static final String DB_PASSWORD = "";
 }
