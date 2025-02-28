bbda2eb58b7731c7dba632b2a12685089f1ebea3
PenguGit <160494880+PenguGit@users.noreply.github.com>
2025-02-28 12:58:46 +0100

    Squashed commit of the following:
    commit f576e7c3e2055d1b1cc7e30825016a5a1671d11f
    Author: PenguGit <160494880+PenguGit@users.noreply.github.com>
    Date:   Fri Feb 28 12:56:43 2025 +0100
    
        Untrack doc directory
    
    commit 0ba6bc02b758ed2fa21ccdf028c15450caf0cd4c
    Author: PenguGit <160494880+PenguGit@users.noreply.github.com>
    Date:   Fri Feb 28 12:53:29 2025 +0100
    
        Update .gitignore
    
    commit 541bd19b80cdd6bab2b7c8ba2e73a433e1e475ac
    Author: PenguGit <160494880+PenguGit@users.noreply.github.com>
    Date:   Fri Feb 28 12:20:47 2025 +0100
    
        More Comments
    
    commit 4ae9fc202d39e03ec91ef143c8e5f64aba4ab73a
    Author: PenguGit <160494880+PenguGit@users.noreply.github.com>
    Date:   Fri Feb 28 12:49:09 2025 +0100
    
        Reapply "More Comments"
    
        This reverts commit 288d205eaf8c1a7dcdc365d6d9a22a5d71691859.
    
    commit 288d205eaf8c1a7dcdc365d6d9a22a5d71691859
    Author: PenguGit <160494880+PenguGit@users.noreply.github.com>
    Date:   Fri Feb 28 12:45:07 2025 +0100
    
        Revert "More Comments"
    
        This reverts commit 966e4f5eb8c3fc9a70cbdf86092e4b6e07412211.

--- Animalshelter/src/bl/entities/EntityDTO.java
+++ Animalshelter/src/bl/entities/EntityDTO.java
@@ -1,5 +1,10 @@
 package bl.entities;
 
+/**
+ * The {@code EntityDTO} class serves as an abstract base class for Data Transfer Objects (DTOs) representing entities in the business logic layer.
+ * It provides a common structure for entity DTOs, including an ID field and associated getter and setter methods.
+ * This class is intended to be extended by specific DTO classes that correspond to data entities and facilitate data transfer between layers of the application.
+ */
 public abstract class EntityDTO {
 	protected int id = -1;
 	
