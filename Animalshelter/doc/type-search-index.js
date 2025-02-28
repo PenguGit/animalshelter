f576e7c3e2055d1b1cc7e30825016a5a1671d11f
PenguGit <160494880+PenguGit@users.noreply.github.com>
2025-02-28 12:56:43 +0100

    Untrack doc directory

--- Animalshelter/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html"},{"p":"gui.animalview","l":"AnimalViewPanel"},{"p":"gui.animalview","l":"AnimalViewPanel.Mode"}];updateSearchResults();
